e717b5ed4ee91899917ea74a87198a1a763b2d65
PqBenoit <benoit@monsieurdrive.com>
2014-10-15 22:52:10 +0200

    AZERTY

--- scripts/coffee/main.coffee
+++ scripts/coffee/main.coffee
@@ -12,10 +12,14 @@ missionAnmialeApp.controller 'HeaderCtrl', [ '$scope', ($scope) ->
 			x = $("#page-2").offset().top
 			
 			if y >= x
+				$('#logo').attr('src', 'img/logo-black-blue.png')
 				header.removeClass('white').addClass('black')
+				console.log y
+
 				return false
 			else
 				header.removeClass('black').addClass('white')
+				$('#logo').attr('src', 'img/logo-black-white.png')
 				return false
 
 			$(target).addClassName('.current')
@@ -26,9 +30,39 @@ missionAnmialeApp.controller 'HeaderCtrl', [ '$scope', ($scope) ->
 			target = $(this).attr('href')
 			width = $(target).offset().top;
 			console.log target
-			$('html, body').animate {scrollTop:width}, 500
+			$('html, body').animate {scrollTop:width}, 750
 			$(target).addClassName('.current')
 			
 			return false
 
+		$('.paragraphs').hide()
+		$('.content-2-1').mouseenter ( ->
+			$('.title-2-1').hide()
+			$('.paragraph-2-1').fadeIn()
+		)
+
+		$('.content-2-1').mouseleave ( ->
+			$('.paragraph-2-1').hide()
+			$('.title-2-1').fadeIn()
+		)
+
+		$('.content-2-2').mouseenter ( ->
+			$('.title-2-2').hide()
+			$('.paragraph-2-2').fadeIn()
+		)
+
+		$('.content-2-2').mouseleave ( ->
+			$('.paragraph-2-2').hide()
+			$('.title-2-2').fadeIn()
+		)
+
+		$('.content-2-3').mouseenter ( ->
+			$('.title-2-3').hide()
+			$('.paragraph-2-3').fadeIn()
+		)
+
+		$('.content-2-3').mouseleave ( ->
+			$('.paragraph-2-3').hide()
+			$('.title-2-3').fadeIn()
+		)
 ]
--- scripts/main.js
+++ scripts/main.js
@@ -13,26 +13,54 @@
           y = $(this).scrollTop();
           x = $("#page-2").offset().top;
           if (y >= x) {
+            $('#logo').attr('src', 'img/logo-black-blue.png');
             header.removeClass('white').addClass('black');
+            console.log(y);
             return false;
           } else {
             header.removeClass('black').addClass('white');
+            $('#logo').attr('src', 'img/logo-black-white.png');
             return false;
           }
           $(target).addClassName('.current');
           return false;
         });
-        return $('a[href^="#"]').click(function() {
+        $('a[href^="#"]').click(function() {
           var target, width;
           target = $(this).attr('href');
           width = $(target).offset().top;
           console.log(target);
           $('html, body').animate({
             scrollTop: width
-          }, 500);
+          }, 750);
           $(target).addClassName('.current');
           return false;
         });
+        $('.paragraphs').hide();
+        $('.content-2-1').mouseenter((function() {
+          $('.title-2-1').hide();
+          return $('.paragraph-2-1').fadeIn();
+        }));
+        $('.content-2-1').mouseleave((function() {
+          $('.paragraph-2-1').hide();
+          return $('.title-2-1').fadeIn();
+        }));
+        $('.content-2-2').mouseenter((function() {
+          $('.title-2-2').hide();
+          return $('.paragraph-2-2').fadeIn();
+        }));
+        $('.content-2-2').mouseleave((function() {
+          $('.paragraph-2-2').hide();
+          return $('.title-2-2').fadeIn();
+        }));
+        $('.content-2-3').mouseenter((function() {
+          $('.title-2-3').hide();
+          return $('.paragraph-2-3').fadeIn();
+        }));
+        return $('.content-2-3').mouseleave((function() {
+          $('.paragraph-2-3').hide();
+          return $('.title-2-3').fadeIn();
+        }));
       });
     }
   ]);
